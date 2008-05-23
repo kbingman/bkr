2e8b5aa399987f5f7e5c1c63bb67057eea53f337
Keith Bingman <keith@Calculon.local>
2008-05-23 08:30:38 +0200

    Made reorder extension work with Admin Tree

--- vendor/disabled_extensions/reorder/lib/reorder_page_helper_extensions.rb
+++
@@ -1,6 +0,0 @@
-module ReorderPageHelperExtensions
-  def page_icon(page)
-    icon = page.virtual? ? "virtual-page" : "page"
-    image(icon, :class => "icon", :alt => 'page-icon', :title => '', :align => 'center')
-  end
-end
